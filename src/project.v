e19be1b372dbee3edb22009cb899e900612e2545
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-09-04 00:28:53 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Armaan Gomes
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 // This porject was a collaboration between me(arghunter) and the other collaborators on this repository (Acknowledged in the ReadMe)
-module tt_um_supermic_arghunter (
+module tt_um_pdm_pitch_filter_arghunter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -19,23 +19,21 @@ wire rst;
   // All output pins must be assigned. If not used, assign to 0.
   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
-  assign uio_oe[4:0]  = 0;
-  assign uio_oe[6:5]  = 1;
-  assign uio_oe[7]  = 0;
+  assign uio_oe[7:0]  = 0;
+  assign uo_out[7:2] = 0;
   assign rst = !rst_n;
   generate
     supermic_top_module u_supermic_top_module (
         .clk(ui_in[0]),
         .rst(rst),
-        .lr_clk(ui_in[1]),
-        .delay_select(uio_in[4:0]),
-        .pdm(ui_in[5:2]),
-        .i2s_out(uio_out[5]),
-        .mic_clk(uio_out[6]),
-        .cic_out(uo_out)
+        .spi_mosi(ui_in[1]),
+        .spi_cs_n(ui_in[2]),
+        .mic_data(ui_in[3]),
+        .out(uo_out[0]),
+        .mic_clk(uo_out[1])
     );    
   endgenerate
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk,ui_in[7:6],uio_in[7], 1'b0};
+    wire _unused = &{ena, clk,uio_in[7:0], 1'b0};
 
 endmodule
